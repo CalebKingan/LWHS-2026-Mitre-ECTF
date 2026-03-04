eb3ae19709239cfae09dc5187810271e4ef6d391
Caleb Kingan <72657641+CalebKingan@users.noreply.github.com>
2026-03-03 23:33:38 -0600

    Remove INVALID_PIN_DELAY_CYCLES definition
    Remove the definition of INVALID_PIN_DELAY_CYCLES.

--- firmware/inc/security.h
+++ firmware/inc/security.h
@@ -19,7 +19,6 @@
 #define MAX_PERMS 8
 #define PIN_LENGTH 6
 #define INVALID_PIN_DELAY 5U
-#define INVALID_PIN_DELAY_CYCLES ((CPUCLK_FREQ / 1000U) * INVALID_PIN_DELAY_MS)
 
 typedef enum {
     PERM_READ = 'R',
